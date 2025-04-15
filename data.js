9d555758de4fd08f74cf9419060b0415ada9ded4
E-RKP <edobackup111@gmail.com>
2025-04-15 17:13:07 +0200

    some changes

--- data.js
+++ data.js
@@ -1,6 +1,27 @@
-let drive = prompt('enter your age')
-if (drive >= 18) {
-  console.log(`Your age is ${drive}, you are old enough to drive`)
-}else {
-  console.log(`Your age is ${drive}, you need to wait ${18-Number(drive)} years to drive`)
+//let drive = prompt('enter your age')
+//if (drive >= 18) {
+//  console.log(`Your age is ${drive}, you are old enough to drive`)
+//}else {
+//  console.log(`Your age is ${drive}, you need to wait ${18-Number(drive)} years to drive`)
+//}
+
+//let myAge = 27
+//let yourAge = prompt('insert your age')
+//if (myAge == yourAge){
+//  console.log('We have the same age')
+//}else if (myAge > yourAge){
+//  console.log(`I am ${myAge-yourAge} years older than you`)
+//}else if (myAge < yourAge){
+//  console.log(`I am ${yourAge-myAge} younger than you`)
+//}
+
+let a = 1
+let b = 1
+// a > b ? console.log('a is greater than b') : console.log('a is greater than b')
+if (a >b) {
+  console.log('a is greater than b')
+}else if (b > a){
+  console.log('a is greater than b')
+}else{
+  console.log('i numeri sono uguali')
 }
